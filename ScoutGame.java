39234765ec20e42a282fd24c23db426709335713
kamkom84 <kbd.dimitrov@gmail.com>
2024-11-30 00:28:49 +0200

    commit msg:

--- ScoutGame.java
+++ ScoutGame.java
@@ -352,7 +352,7 @@ public class ScoutGame extends JFrame {
                 positionIsValid = !isNearBase(x, y) && !isNearWorkers(x, y, workerPositions);
             } while (!positionIsValid);
 
-            resources[i] = new Resource(x, y, 200);////////////////////////////////////////////////////////////////
+            resources[i] = new Resource(x, y, 20);////////////////////////////////////////////////////////////////
         }
     }
 
